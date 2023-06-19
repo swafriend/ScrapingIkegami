07c101f5e65fe82dccaedfc36175e3a0ae85552f
SWA <wadasadamu@gmail.com>
2023-06-20 00:27:11 +0900

    single execute

--- src/main/java/com/example/scraping1/Scraping1Application.java
+++ src/main/java/com/example/scraping1/Scraping1Application.java
@@ -31,7 +31,8 @@ public class Scraping1Application {
         SpringApplication.run(Scraping1Application.class, args);
 
         String thePath = "C:\\Windows\\Media\\Windows Ringin.wav";
-        try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(thePath).getAbsoluteFile())) {
+        try (AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(thePath).getAbsoluteFile())) {
+
             String LOGIN_URL = "https://www.e-license.jp/el31/mSg1DWxRvAI-brGQYS-1OA==";
             String mainUrl = "https://www.e-license.jp/el31/pc/login";
 //            String reserveUrl = "https://www.e-license.jp/el31/pc/reserv/p03/p03a";
@@ -42,64 +43,65 @@ public class Scraping1Application {
             final String USERNAME = myapp.getUser();
             final String PASS = myapp.getPassword();
 
-            for (int vvv = 1; vvv < myapp.getLoop_count(); vvv++) {
+//            for (int vvv = 1; vvv < myapp.getLoop_count(); vvv++) {
 
-                Map<String, String> cookies = new HashMap<>();
-                Connection.Response response1 = Jsoup.connect(LOGIN_URL)
-                        .method(Connection.Method.GET)
-                        .execute();
-                Connection.Response response2 = Jsoup.connect(mainUrl)
-                        .data("schoolCd", SCHOOLID)
-                        .data("studentId", USERNAME)
-                        .data("password", PASS)
-                        .userAgent(UA)
-                        .cookies(response1.cookies())
-                        .followRedirects(false)
-                        .method(Connection.Method.POST)
-                        .execute();
+            Map<String, String> cookies = new HashMap<>();
+            Connection.Response response1 = Jsoup.connect(LOGIN_URL)
+                    .method(Connection.Method.GET)
+                    .execute();
+            Connection.Response response2 = Jsoup.connect(mainUrl)
+                    .data("schoolCd", SCHOOLID)
+                    .data("studentId", USERNAME)
+                    .data("password", PASS)
+                    .userAgent(UA)
+                    .cookies(response1.cookies())
+                    .followRedirects(false)
+                    .method(Connection.Method.POST)
+                    .execute();
 
-                response1.cookies().forEach((s, s2) -> {
-                            if (!s.equals("JSESSIONID")) {
-                                cookies.put(s, s2);
-                            }
+            response1.cookies().forEach((s, s2) -> {
+                        if (!s.equals("JSESSIONID")) {
+                            cookies.put(s, s2);
                         }
-                );
-                response2.cookies().forEach((s, s2) -> {
-                            if (s.equals("JSESSIONID")) {
-                                cookies.put(s, s2);
-                            }
-                        }
-                );
-
-                // リダイレクト先のURLを取得
-                String urlRedirectPath = "https://www.e-license.jp" + Objects.requireNonNull(response2.header("Location"));
-                Document document = Jsoup.connect(urlRedirectPath)
-                        .userAgent(UA)
-                        .cookies(cookies)
-                        .timeout(10000)
-                        .referrer(LOGIN_URL)
-                        .followRedirects(false)
-                        .get();
-
-                Elements elements = document.getAllElements();
-                for(Element element:elements){
-                    if (element.className().equals("status1")) {
-                        // the reference to the clip
-                        Clip  clip = AudioSystem.getClip();
-                        clip.open(audioStream);
-                        clip.loop(10);
-                        log.info("空きがありました。音を鳴らしました。");
-                        break;
                     }
+            );
+            response2.cookies().forEach((s, s2) -> {
+                        if (s.equals("JSESSIONID")) {
+                            cookies.put(s, s2);
+                        }
+                    }
+            );
+
+            // リダイレクト先のURLを取得
+            String urlRedirectPath = "https://www.e-license.jp" + Objects.requireNonNull(response2.header("Location"));
+            Document document = Jsoup.connect(urlRedirectPath)
+                    .userAgent(UA)
+                    .cookies(cookies)
+                    .timeout(10000)
+                    .referrer(LOGIN_URL)
+                    .followRedirects(false)
+                    .get();
+
+            Elements elements = document.getAllElements();
+            for (Element element : elements) {
+                if (element.className().equals("status1")) {
+                    // the reference to the clip
+                    Clip clip = AudioSystem.getClip();
+                    clip.open(audioStream);
+                    clip.loop(10);
+                    log.info("空きがありました。音を鳴らしました。");
+                    break;
                 }
-
-                log.info("ループ回数：" + vvv);
-
-                Thread.sleep(myapp.getScraping());
             }
 
-        } catch (IOException | InterruptedException | UnsupportedAudioFileException | LineUnavailableException e) {
-            log.error("エラーが発生しました。" ,e);
+            Thread.sleep(myapp.getScraping());
+            log.info("終了しました。");
+//            log.info("ループ回数：" + vvv);
+//            Thread.sleep(myapp.getScraping());
+//            }
+
+        } catch (IOException | UnsupportedAudioFileException | LineUnavailableException | InterruptedException e) {
+            log.error("エラーが発生しました。", e);
         }
 
         log.info("プロセスを終了します。");
